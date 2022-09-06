38527a55bb13ddfa54809f0149eb1801d0c2b4c3
danburudoa <yumamukai515@gmail.com>
2022-09-06 16:34:45 +0900

    遷移先を指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  # 以下の記述でcurrent_userがuserがviewでも使えるようになる
+  helper_method :current_user
+
+  # ログインしていない人の遷移先を指定
+  def login?
+    if current_user.nil?
+      redirect_to login_path, alert: 'ログインが必要です。'
+    end
+  end
+
+  # すでにログインしている人の遷移先を指定
+  def already_login?
+    unless current_user.nil?
+      redirect_to user_path, notice: '既にログインしています。'
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      current_user ||= User.find(session[:user_id])
+    end
+  end
 end
